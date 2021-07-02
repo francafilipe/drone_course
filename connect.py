e4380ed797f23b774f7967a325b5ecad82ecf97f
francafilipe <filipeffranc@gmail.com>
2021-07-02 10:34:13 -0300

    Add automatic start for sitl when ip adress not passed

--- connect.py
+++ connect.py
@@ -3,6 +3,13 @@ from dronekit import connect
 
 # -------- Function to Connect to a vehicle --------
 def connectCopter(conn_ip):
+    # Launches SITL drone if non IP address was explicitly specified
+    if not conn_ip:
+        import dronekit_sitl
+        sitl = dronekit_sitl.start_default()
+        conn_ip = sitl.connection_string()
+
+    print(conn_ip)
     # Connects to a drone listening in the conn_ip IP address
     copter = connect(conn_ip,wait_ready=True)
 
